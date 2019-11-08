0f8c37d5144731156157536c33ea58c5d41021e8
Oizys18 <Oizys18@gmail.com>
2019-11-08 18:11:03 +0900

    First commit

---
+++ movie/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <!-- 1-3. 호출하시오. 
+        필요한 경우 props를 데이터를 보내줍니다.
+      -->
+      <MovieList :movies='movies' :genres='genres'/>
+    </div>
+  </div>
+</template>
+
+<script>
+const axios = require('axios');
+// 1-1. 저장되어 있는 MovieList 컴포넌트를 불러오고,
+import MovieList from "./components/movies/MovieList"
+export default {
+  name: 'app',
+  // 1-2. 아래에 등록 후
+  components: {
+    MovieList,
+  },
+  data() {
+    return {
+      // 활용할 데이터를 정의하시오.
+      movies: [],
+      genres: [],
+    }
+  },
+
+  mounted() {
+    console.log('Hi, The app is running')
+    const MOVIESURL = "https://gist.githubusercontent.com/edu-john/9b7645f841e402ce7fab2b8b606b7c7e/raw/271285772e7dfbb9b476eb27e1b85ead683d763c/movies.json"
+    const GENRESURL = "https://gist.githubusercontent.com/djohnkang/7cc43f82b5165779819e3b6ad299965b/raw/b0b5850b55806672cde4666e544b6840548159a2/genres.json"
+    // 0. mounted 되었을 때, 
+    // 1) 제시된 URL로 요청을 통해 data의 movies 배열에 해당 하는 데이터를 넣으시오. 
+    // 2) 제시된 URL로 요청을 통해 data의 genres 배열에 해당 하는 데이터를 넣으시오.
+    // axios는 위에 호출되어 있으며, node 설치도 완료되어 있습니다.
+    axios.get(MOVIESURL)
+      .then(result => {
+        this.movies = result.data
+        // .movies=(result.data)
+      })
+    axios.get(GENRESURL)
+      .then(result => {
+        this.genres = result.data
+      })
+  },
+
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ movie/src/components/movies/MovieListItem.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="col-3 my-3">
+    <!-- img 태그에 src와 alt값(영화제목)을 설정하시오 -->
+    <img class="movie--poster my-3" :src="movie.poster_url" alt="movie.poster_url" />
+    <!-- 영화 제목을 출력하시오. -->
+    <h5>{{ movie.name }}</h5>
+    <!-- 모달을 활용하기 위해서는 data-taget에 모달에서 정의된 id값을 넣어야 합니다. -->
+    <button class="btn btn-primary" data-toggle="modal" :data-target="movieid" >영화 정보 상세보기</button>
+    <!-- 1-3. 호출하시오.필요한 경우 props를 데이터를 보내줍니다.-->
+    <MovieListItemModal :movie='movie' /> 
+    <!-- :modal='modal' -->
+  </div>
+</template>
+
+<script>
+console.log('MovieListItem - loaded')
+// 1-1. 저장되어 있는 MovieListItemModal 컴포넌트를 불러오고,
+import MovieListItemModal from "./MovieListItemModal";
+export default {
+  name: "MovieListItem",
+  // 1-2. 아래에 등록 후
+  components: {
+    MovieListItemModal
+  },
+  // 0. props 데이터를 받기 위하여 설정하시오.
+  // movie 타입은 Object이며, 필수입니다.
+  // 설정이 완료 되었다면, 상위 컴포넌트에서 값을 넘겨 주세요.
+  // 그리고 적절한 곳에 사용하세요.
+  props: {
+    movie: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    // 활용할 데이터를 정의하시오.
+    return{
+      movieid: `#movie-${this.movie.id}`,
+    }
+  },
+  // methods:{
+  //   showModal() {
+  //     this.modal = true
+  //   }
+  // }
+};
+</script>
+
+<style>
+.movie--poster {
+  width: 200px;
+}
+</style>
